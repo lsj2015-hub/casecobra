3af0c012c30eac59dfe7bd04b7b1e83ceebcfbf0
lsj2015-hub <lsj2015@hotmail.com>
2024-05-19 17:43:26 +0800

    Creaing our stripe webhook

--- src/app/configure/preview/actions.ts
+++ src/app/configure/preview/actions.ts
@@ -45,7 +45,7 @@ export const createCheckoutSession = async ({
     },
   });
 
-  console.log(user.id, configuration.id);
+  // console.log(user.id, configuration.id);
 
   if (existingOrder) {
     order = existingOrder;
