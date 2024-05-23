0854c22bbebcef88b54d6a44136661369fad3370
lsj2015-hub <lsj2015@hotmail.com>
2024-05-23 18:27:14 +0800

    Writing our thank-you email by using react-email, resend library

--- src/app/configure/design/design-configurator.tsx
+++ src/app/configure/design/design-configurator.tsx
@@ -46,7 +46,7 @@ const DesignConfigurator = ({
   const { toast } = useToast();
   const router = useRouter();
 
-  const { mutate: saveConfig } = useMutation({
+  const { mutate: saveConfig, isPending } = useMutation({
     mutationKey: ['save-config'],
     mutationFn: async (args: SaveConfigArgs) => {
       await Promise.all([saveConfiguration(), _saveConfig(args)]);
@@ -389,6 +389,9 @@ const DesignConfigurator = ({
                 )}
               </p>
               <Button
+                isLoading={isPending}
+                disabled={isPending}
+                loadingText="Saving"
                 onClick={() =>
                   saveConfig({
                     configId,
